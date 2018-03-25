22f3afea738a3cd24078d9aee5932896c43d8785
zhoucheng_git <zhoucheng05_13@163.com>
2018-03-25 19:34:49 +0800

    AbstractCollection阅读注释完毕

--- JDK1.9/src/java.base/java/util/AbstractCollection.java
+++ JDK1.9/src/java.base/java/util/AbstractCollection.java
@@ -26,24 +26,30 @@
 package java.util;
 
 /**
+ * 该类提供了Collection的一个提纲性的实现，以减小直接实现Colleciotn接口的影响
  * This class provides a skeletal implementation of the {@code Collection}
  * interface, to minimize the effort required to implement this interface. <p>
  *
+ *  如果程序员想实现一个不可变的集合，那么它只需要继承本类，并实现iterator和size方法
  * To implement an unmodifiable collection, the programmer needs only to
  * extend this class and provide implementations for the {@code iterator} and
  * {@code size} methods.  (The iterator returned by the {@code iterator}
  * method must implement {@code hasNext} and {@code next}.)<p>
  *
+ * 如果要实现可修改集合，那么编程者必须要额外重写本类的add方法。返回的iterator也必须实现
+ * remove方法
  * To implement a modifiable collection, the programmer must additionally
  * override this class's {@code add} method (which otherwise throws an
  * {@code UnsupportedOperationException}), and the iterator returned by the
  * {@code iterator} method must additionally implement its {@code remove}
  * method.<p>
  *
+ *
  * The programmer should generally provide a void (no argument) and
- * {@code Collection} constructor, as per the recommendation in the
+ * {@code Collection} constructor, as per（as per按照） the recommendation in the
  * {@code Collection} interface specification.<p>
  *
+ *  所有抽象方法的说明文档都包含了该方法的实现细节。所有的这些方法都可以被重写。
  * The documentation for each non-abstract method in this class describes its
  * implementation in detail.  Each of these methods may be overridden if
  * the collection being implemented admits a more efficient implementation.<p>
@@ -60,7 +66,7 @@ package java.util;
 
 public abstract class AbstractCollection<E> implements Collection<E> {
     /**
-     * Sole constructor.  (For invocation by subclass constructors, typically
+     * Sole（唯一的） constructor.  (For invocation by subclass constructors, typically
      * implicit.)
      */
     protected AbstractCollection() {
@@ -69,6 +75,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     // Query Operations
 
     /**
+     * 返回迭代器，需要子类实现该方法
      * Returns an iterator over the elements contained in this collection.
      *
      * @return an iterator over the elements contained in this collection
@@ -89,6 +96,8 @@ public abstract class AbstractCollection<E> implements Collection<E> {
 
     /**
      * {@inheritDoc}
+     *实现方式：通过迭代器依次遍历该集合，一旦发现满足equal()方法的元素，就返回true。
+     * 如果遍历完还没有找到，就返回false。
      *
      * @implSpec
      * This implementation iterates over the elements in the collection,
@@ -113,7 +122,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
 
     /**
      * {@inheritDoc}
-     *
+     * 将当前集合元素拷贝到一个新数组中去，
      * @implSpec
      * This implementation returns an array containing all the elements
      * returned by this collection's iterator, in the same order, stored in
@@ -149,10 +158,13 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     /**
      * {@inheritDoc}
      *
+     * 在并发情况下，这个操作的结果也是将会是正确的，因为size方法只是作为一个优化提示
+     * 当运行过程中发现数组大小不够时，会重新创建一个容量newCap = cap + (cap >> 1) + 1;大小的新数组，
+     * 并将所有元素复制进去。
      * @implSpec
      * This implementation returns an array containing all the elements
      * returned by this collection's iterator in the same order, stored in
-     * consecutive elements of the array, starting with index {@code 0}.
+     * consecutive(连续的) elements of the array, starting with index {@code 0}.
      * If the number of elements returned by the iterator is too large to
      * fit into the specified array, then the elements are returned in a
      * newly allocated array with length equal to the number of elements
@@ -204,6 +216,8 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     }
 
     /**
+     * 一些虚拟机在数组中存储了限制大小的头字段。如果尝试分配更大的空间
+     * 会导致出现outOfMemmoryException
      * The maximum size of array to allocate.
      * Some VMs reserve some header words in an array.
      * Attempts to allocate larger arrays may result in
@@ -212,6 +226,9 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
 
     /**
+     * 用于在toArray方法中，当数组容量不够时，重新分配数组的大小，并将迭代器中
+     * 剩余的元素复制到新数组中去。扩容后大小分两种情况，一种是元素容量的3倍+1，
+     * 另一种则是Integer.MAX_VALUE。
      * Reallocates the array being used within toArray when the iterator
      * returned more elements than expected, and finishes filling it from
      * the iterator.
@@ -222,6 +239,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      *         further elements returned by the iterator, trimmed to size
      */
     @SuppressWarnings("unchecked")
+
     private static <T> T[] finishToArray(T[] r, Iterator<?> it) {
         int i = r.length;
         while (it.hasNext()) {
@@ -252,7 +270,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
 
     /**
      * {@inheritDoc}
-     *
+     * AbstractColleciton不支持直接添加对象的操作
      * @implSpec
      * This implementation always throws an
      * {@code UnsupportedOperationException}.
@@ -269,12 +287,13 @@ public abstract class AbstractCollection<E> implements Collection<E> {
 
     /**
      * {@inheritDoc}
-     *
+     * 通过迭代器遍历所有元素来寻找传入的对象。如果找到了就使用迭代器的remove方法移除该元素
      * @implSpec
      * This implementation iterates over the collection looking for the
      * specified element.  If it finds the element, it removes the element
      * from the collection using the iterator's remove method.
      *
+     * 如果该集合的子类实现中存在传入的对象，但是迭代器不支持remove方法，那么会抛出异常
      * <p>Note that this implementation throws an
      * {@code UnsupportedOperationException} if the iterator returned by this
      * collection's iterator method does not implement the {@code remove}
@@ -310,6 +329,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     /**
      * {@inheritDoc}
      *
+     * 依次遍历传入集合中的元素，并查找本集合中是否存在匹配的元素，如果不存在，则直接返回false
      * @implSpec
      * This implementation iterates over the specified collection,
      * checking each element returned by the iterator in turn to see
@@ -330,6 +350,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     /**
      * {@inheritDoc}
      *
+     * 只有它的子类重写了add(E e)方法时，该方法才可用。
      * @implSpec
      * This implementation iterates over the specified collection, and adds
      * each object returned by the iterator to this collection, in turn.
@@ -357,6 +378,8 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     /**
      * {@inheritDoc}
      *
+     * 依次移除通传入集合中相同的元素。如果删除了至少一个元素，那么返回true，否则返回false
+     * 删除元素是使用的Iterator.remove方法。
      * @implSpec
      * This implementation iterates over this collection, checking each
      * element returned by the iterator in turn to see if it's contained
@@ -426,6 +449,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
 
     /**
      * {@inheritDoc}
+     * 通过迭代器依次移除集合的元素
      *
      * @implSpec
      * This implementation iterates over this collection, removing each
@@ -452,6 +476,7 @@ public abstract class AbstractCollection<E> implements Collection<E> {
     //  String conversion
 
     /**
+     * 返回所有元素构成的的字符串
      * Returns a string representation of this collection.  The string
      * representation consists of a list of the collection's elements in the
      * order they are returned by its iterator, enclosed in square brackets
