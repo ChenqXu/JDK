b5a7d4704a3495bf125704db1cd8821d3b4ae901
zhoucheng_git <zhoucheng05_13@163.com>
2018-03-28 11:40:23 +0800

    BlockingQueue阅读注释完

--- JDK1.9/src/java.base/java/util/concurrent/BlockingQueue.java
+++ JDK1.9/src/java.base/java/util/concurrent/BlockingQueue.java
@@ -39,14 +39,23 @@ import java.util.Collection;
 import java.util.Queue;
 
 /**
+ * Queue的一个子接口，它支持如下操作：
+ * 当执行出队操作时，如果队列为空，则等待
+ * 当执行如队操作时，如果队列已满，则等待
  * A {@link java.util.Queue} that additionally supports operations
  * that wait for the queue to become non-empty when retrieving an
  * element, and wait for space to become available in the queue when
  * storing an element.
  *
+ * 阻塞队列的方法有四种形式，每种方式返回的结果不同，以处理不同的情形。
  * <p>{@code BlockingQueue} methods come in four forms, with different ways
  * of handling operations that cannot be satisfied immediately, but may be
  * satisfied at some point in the future:
+ *
+ * 一种抛出异常，
+ * 第二种返回一个特别的值（通常是null或false），
+ * 第三种无限期阻塞当前的线程，直到条件满足（有元素或有空间），
+ * 第四种仅阻塞一个指定的时间。
  * one throws an exception, the second returns a special value (either
  * {@code null} or {@code false}, depending on the operation), the third
  * blocks the current thread indefinitely until the operation can succeed,
@@ -85,17 +94,25 @@ import java.util.Queue;
  *  </tr>
  * </table>
  *
+ * 阻塞队列不接受null值，如果传入null值，那么会抛出NullPointerException异常
  * <p>A {@code BlockingQueue} does not accept {@code null} elements.
  * Implementations throw {@code NullPointerException} on attempts
  * to {@code add}, {@code put} or {@code offer} a {@code null}.  A
  * {@code null} is used as a sentinel value to indicate failure of
  * {@code poll} operations.
  *
+ *  一个阻塞队列通常是容量有限的
  * <p>A {@code BlockingQueue} may be capacity bounded. At any given
  * time it may have a {@code remainingCapacity} beyond which no
  * additional elements can be {@code put} without blocking.
+ *
+ * 一个没有容量限制的阻塞队列的remaining capacity属性总是为Integer.MAX_VALUE
  * A {@code BlockingQueue} without any intrinsic capacity constraints always
  * reports a remaining capacity of {@code Integer.MAX_VALUE}.
+ *
+ *  阻塞队列接口的实现，通过被用作生产者-消费者队列。但同时它还具有Collection集合的
+ *  方法。因此，还可以通过Remove(x)方法移除队列中的任意一个元素。但是，类似的操作
+ *  效率并不高，并且只是偶尔使用（如当一个队列信息被取消时）。
  *
  * <p>{@code BlockingQueue} implementations are designed to be used
  * primarily for producer-consumer queues, but additionally support
@@ -105,6 +122,9 @@ import java.util.Queue;
  * <em>not</em> performed very efficiently, and are intended for only
  * occasional use, such as when a queued message is cancelled.
  *
+ * BlockingQueue是线程安全的，所有的队列操作都自动地使用内部锁或其他同步控制。
+ * 然而，涉及到大量元素操作的Colleciotn方法，如addAll、containsAll、retainAll等
+ *并不会自动地同步，除非具体的实现自行保证。因此，addAll这类方法可能会在执行过程中失败。
  * <p>{@code BlockingQueue} implementations are thread-safe.  All
  * queuing methods achieve their effects atomically using internal
  * locks or other forms of concurrency control. However, the
@@ -115,6 +135,8 @@ import java.util.Queue;
  * {@code addAll(c)} to fail (throwing an exception) after adding
  * only some of the elements in {@code c}.
  *
+ * 一个BlockingQueue本身并不支持close、shutdown等表明不能再添加元素的操作。
+ * 这些特性由需要的子类去自行实现。
  * <p>A {@code BlockingQueue} does <em>not</em> intrinsically support
  * any kind of &quot;close&quot; or &quot;shutdown&quot; operation to
  * indicate that no more items will be added.  The needs and usage of
@@ -123,8 +145,9 @@ import java.util.Queue;
  * <em>end-of-stream</em> or <em>poison</em> objects, that are
  * interpreted accordingly when taken by consumers.
  *
+ * 以下是基于典型生产者消费者场景的BlockingQueue使用示例。
  * <p>
- * Usage example, based on a typical producer-consumer scenario.
+ * Usage example, based on a typical producer-consumer scenario（场景）.
  * Note that a {@code BlockingQueue} can safely be used with multiple
  * producers and multiple consumers.
  * <pre> {@code
@@ -162,6 +185,8 @@ import java.util.Queue;
  *   }
  * }}</pre>
  *
+ * 内存一致性效果：在一个线程中对BlockingQueue的插入操作，先行于其他线程中对同一个元素的读取
+ * 和移除操作
  * <p>Memory consistency effects: As with other concurrent
  * collections, actions in a thread prior to placing an object into a
  * {@code BlockingQueue}
@@ -277,6 +302,7 @@ public interface BlockingQueue<E> extends Queue<E> {
         throws InterruptedException;
 
     /**
+     * 返回当前BlockingQueueQueue还能插入的元素数量
      * Returns the number of additional elements that this queue can ideally
      * (in the absence of memory or resource constraints) accept without
      * blocking, or {@code Integer.MAX_VALUE} if there is no intrinsic
@@ -292,6 +318,7 @@ public interface BlockingQueue<E> extends Queue<E> {
     int remainingCapacity();
 
     /**
+     * 删除队列中的元素o。
      * Removes a single instance of the specified element from this queue,
      * if it is present.  More formally, removes an element {@code e} such
      * that {@code o.equals(e)}, if this queue contains one or more such
@@ -310,6 +337,7 @@ public interface BlockingQueue<E> extends Queue<E> {
     boolean remove(Object o);
 
     /**
+     * 判断队列中是否有该元素。
      * Returns {@code true} if this queue contains the specified element.
      * More formally, returns {@code true} if and only if this queue contains
      * at least one element {@code e} such that {@code o.equals(e)}.
@@ -325,6 +353,7 @@ public interface BlockingQueue<E> extends Queue<E> {
     boolean contains(Object o);
 
     /**
+     * 将队列中的所有元素移除并添加到传入的集合c中。这个操作比重复地调用poll更加有效
      * Removes all available elements from this queue and adds them
      * to the given collection.  This operation may be more
      * efficient than repeatedly polling this queue.  A failure
@@ -350,6 +379,7 @@ public interface BlockingQueue<E> extends Queue<E> {
     int drainTo(Collection<? super E> c);
 
     /**
+     * 移除给定个数的元素，并将其添加到初入的集合c中
      * Removes at most the given number of available elements from
      * this queue and adds them to the given collection.  A failure
      * encountered while attempting to add elements to
