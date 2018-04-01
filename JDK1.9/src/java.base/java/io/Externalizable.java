0261d19f5765747ede09f8dcd91d9debddf8cbcf
zhoucheng_git <zhoucheng05_13@163.com>
2018-04-01 22:42:42 +0800

    Serializable、Externalizable——Java序列化的两种方式——阅读注释完

--- JDK1.9/src/java.base/java/io/Externalizable.java
+++ JDK1.9/src/java.base/java/io/Externalizable.java
@@ -29,16 +29,26 @@ import java.io.ObjectOutput;
 import java.io.ObjectInput;
 
 /**
+ * 实现了该接口的类，需要自己承担保存和恢复实例内容的责任。
  * Only the identity of the class of an Externalizable instance is
  * written in the serialization stream and it is the responsibility
  * of the class to save and restore the contents of its instances.
  *
+ * 子类通过实现Externalizable的writeExternal和readExternal方法来完善对序列化
+ * 和反序列化内容和格式的控制。
  * The writeExternal and readExternal methods of the Externalizable
  * interface are implemented by a class to give the class complete
  * control over the format and contents of the stream for an object
+ * 这些方法必须显式地与父类型协调以保存其状态。这些方法取代了对writeObject和readObject方法的自定义实现
+
  * and its supertypes. These methods must explicitly
  * coordinate with the supertype to save its state. These methods supersede
  * customized implementations of writeObject and readObject methods.<br>
+ *
+ * 对象序列化机制使用Serializable和Externalizable接口。对象持久化机制也可以使用它们。
+ * 在实现了Externalizable接口的类中，每一个被存储的对象都要经过Externalizable接口的测试。
+ * 如果这个对象支持Externalizable，那么该接口的writeExternal将会被调用。如果这个对象不支持
+ * writeExternal但是实现了Serializable接口，那么这个对象将会通过ObjectOutputStream进行存储。
  *
  * Object Serialization uses the Serializable and Externalizable
  * interfaces.  Object persistence mechanisms can use them as well.  Each
@@ -46,11 +56,15 @@ import java.io.ObjectInput;
  * the object supports Externalizable, the writeExternal method is called. If the
  * object does not support Externalizable and does implement
  * Serializable, the object is saved using
+ * 当一个实现了Externalizable接口的对象被反序列化时，会首先调用该类的无参构造器创建一个对象，
+ * 然后调用readExternal读入状态进行装填。而实现了Serializable接口的对象则是通过ObjectInputStream
+ * 来读入对象的。
  * ObjectOutputStream. <br> When an Externalizable object is
  * reconstructed, an instance is created using the public no-arg
  * constructor, then the readExternal method called.  Serializable
  * objects are restored by reading them from an ObjectInputStream.<br>
  *
+ * 一个Externalizable类型的实例可以通过writeReplace()方法和readResolve()来指定在实例化过程中的替代对象。
  * An Externalizable instance can designate a substitution object via
  * the writeReplace and readResolve methods documented in the Serializable
  * interface.<br>
@@ -65,6 +79,8 @@ import java.io.ObjectInput;
  */
 public interface Externalizable extends java.io.Serializable {
     /**
+     * 子类需要实现writeExternal方法来保存其内容，该方法是通过调用其原始值的DataOutput方法，
+     * 或者调用对象、字符串和数组的ObjectOutput的writeObject方法。
      * The object implements the writeExternal method to save its contents
      * by calling the methods of DataOutput for its primitive values or
      * calling the writeObject method of ObjectOutput for objects, strings,
@@ -82,6 +98,9 @@ public interface Externalizable extends java.io.Serializable {
     void writeExternal(ObjectOutput out) throws IOException;
 
     /**
+     * 该方法通过调用原始类型的DataInput方法，或者对象、字符串、数组等引用类型的
+     * readObject方法来读入字节流，并恢复对象的状态。
+     * readExternal方法必须按照与writeExternal方法相同的顺序和类型来读取数据
      * The object implements the readExternal method to restore its
      * contents by calling the methods of DataInput for primitive
      * types and readObject for objects, strings and arrays.  The
--- JDK1.9/src/java.base/java/io/Serializable.java
+++ JDK1.9/src/java.base/java/io/Serializable.java
@@ -26,6 +26,10 @@
 package java.io;
 
 /**
+ * Serializable接口只是一个标记接口，里面的接口方法都是可选的（可实现可不实现，
+ * 如果不实现则启用其自动序列化功能），接口方法并没有实际定义，见说明文档
+ * 实现该接口的类将被允许进行序列化。没有实现该接口的类将不会获得任何序列化或者反序列化的状态
+ * 所有实现该接口的类，都是本身可序列化的。
  * Serializability of a class is enabled by the class implementing the
  * java.io.Serializable interface. Classes that do not implement this
  * interface will not have any of their state serialized or
@@ -33,6 +37,9 @@ package java.io;
  * serializable.  The serialization interface has no methods or fields
  * and serves only to identify the semantics of being serializable. <p>
  *
+ * 为了允许非序列化类的子类型进行序列化（例如，子类实现了Serializable接口，但是父类并没有
+ * 实现），子类型需要承担父类的public、protected和package访问权限字段的存储和恢复状态。
+ * 因此，需要父类提供一个无参的构造器，以便子类进行状态的初始化。
  * To allow subtypes of non-serializable classes to be serialized, the
  * subtype may assume responsibility for saving and restoring the
  * state of the supertype's public, protected, and (if accessible)
@@ -42,21 +49,29 @@ package java.io;
  * Serializable if this is not the case.  The error will be detected at
  * runtime. <p>
  *
+ * 在反序列化期间，非序列化类的字段将会被public或者protected类型的无参构造器初始化。
+ * 父类的构造器必须要被可序列化的子类所访问。可序列化子类的字段将会被流恢复。
+ *
  * During deserialization, the fields of non-serializable classes will
  * be initialized using the public or protected no-arg constructor of
  * the class.  A no-arg constructor must be accessible to the subclass
  * that is serializable.  The fields of serializable subclasses will
  * be restored from the stream. <p>
  *
+ * 当遍历一个图，可能会遇到一个不支持序列化接口的对象。在这种情况下，将会抛出NotSerializableException
+ * 并标志该类为不可序列化。
  * When traversing a graph, an object may be encountered that does not
  * support the Serializable interface. In this case the
  * NotSerializableException will be thrown and will identify the class
  * of the non-serializable object. <p>
  *
+ *
+ *
  * Classes that require special handling during the serialization and
  * deserialization process must implement special methods with these exact
  * signatures:
  *
+ * 一些在序列化或者反序列化期间需要做一些特别操作的类，必须实现下面这些完整签名的方法：
  * <PRE>
  * private void writeObject(java.io.ObjectOutputStream out)
  *     throws IOException
@@ -65,6 +80,13 @@ package java.io;
  * private void readObjectNoData()
  *     throws ObjectStreamException;
  * </PRE>
+ * writeObject()方法用于将对象的当前状态写入到一个对象输出流中，并由输出流决定余下的操作（如，是输出到
+ * 本地文件还是网络中）
+ * readObject()负责从指定对象输入流中读出并恢复类的字段。该方法可能会使用in.defaultReadObject来
+ * 调用默认逻辑恢复对象的非static和非transient字段。
+ * readObjectNoData()方法用于一个并不常见的情景：序列化者（写者）并没有父类，但是反序列化者（读者）有父类，
+ * 在这种情况下，从输入流中不能得到父类的信息。此时，readObjectNoData()方法将会调用父类的构造器创建一个
+ * 新的父类，使得反序列化得以进行。https://blog.csdn.net/dont27/article/details/38309061
  *
  * <p>The writeObject method is responsible for writing the state of the
  * object for its particular class so that the corresponding
@@ -96,13 +118,14 @@ package java.io;
  * party, and the receiver's version extends classes that are not extended by
  * the sender's version.  This may also occur if the serialization stream has
  * been tampered; hence, readObjectNoData is useful for initializing
- * deserialized objects properly despite a "hostile" or incomplete source
+ * deserialized objects properly despite（尽管） a "hostile" or incomplete source
  * stream.
  *
- * <p>Serializable classes that need to designate an alternative object to be
- * used when writing an object to the stream should implement this
- * special method with the exact signature:
  *
+ * <p>Serializable classes that need to designate（指定） an alternative（替代的）object
+ * to be used when writing an object to the stream should implement this
+ * special method with the exact signature:
+ *如果一个类在序列化时，需要指定一个替代对象写入流中，他应该实现下面这个精确定义的方法：
  * <PRE>
  * ANY-ACCESS-MODIFIER Object writeReplace() throws ObjectStreamException;
  * </PRE><p>
@@ -116,6 +139,13 @@ package java.io;
  * Classes that need to designate a replacement when an instance of it
  * is read from the stream should implement this special method with the
  * exact signature.
+ * 保护性恢复对象（同时也可以替换对象）——readResolve
+ * readResolve会在readObject调用之后自动调用，它最主要的目的就是让恢复的对象变个样，
+ * 比如readObject已经反序列化好了一个Person对象，那么就可以在readResolve里再对该对象进行一定的修改，
+ * 而最终修改后的结果将作为ObjectInputStream的readObject的返回结果；其最重要的应用就是保护性恢复
+ * 自己手动实现的单例、枚举类型的对象（Java 5之后的版本都实现了enum类型的自动保护性恢复，但是Java
+ * 5之前的老版本还是不行！）
+ * https://blog.csdn.net/lirx_tech/article/details/51303966
  *
  * <PRE>
  * ANY-ACCESS-MODIFIER Object readResolve() throws ObjectStreamException;
@@ -123,6 +153,24 @@ package java.io;
  *
  * This readResolve method follows the same invocation rules and
  * accessibility rules as writeReplace.<p>
+ *
+ * serialVersionUID是Java序列化中用来验证版本一致性的标志。
+ * 在进行反序列化时，JVM会把传来的字节流中的serialVersionUID与本地相应实体（类）的
+ * serialVersionUID进行比较，如果相同就认为是一致的，可以进行反序列化，否则就会出现序列
+ * 化版本不一致的异常。(InvalidCastException)
+ * 一个可序列化的类可以通过设置一个名为serialVersionUID的字段来显示设置自己的serialVersionUID。
+ * 这个字段必须严格遵循以下格式：
+ * ANY-ACCESS-MODIFIER static final long serialVersionUID = 42L;
+ * 如果一个类没有显示声明自己的serialVersionUID，那么运行时序列化系统会根据类的当前状态生成
+ * 一个64位的hash码作为默认的serialVersionUID。
+ * 然而，所有的可序列化类都被强烈建议显示声明自己的serialVersionUID，因为默认的serialVersionUID
+ * 对那些编译器相关的细节高度敏感，这可能会导致在反序列化时产生意外的InvalidClassException。
+ * 因此，为了保证在不同编译环境下serialVersionUID的一致性，一个可序列化类必须显示声明自己的
+ * serialVersionUID值。
+ * 同时，也强烈建议将serialVersionUID设置为private访问权限，这样，这个serialVersionUID就是类型
+ * 唯一绑定的了，即使它的子类也不会使用该字段。
+ * 数组（Array）类不能显示设置serialVersionUID，因此它们总是使用默认的serialVersionUID。但是
+ * 数组类并不依赖serialVersionUID进行匹配。
  *
  * The serialization runtime associates with each serializable class a version
  * number, called a serialVersionUID, which is used during deserialization to
